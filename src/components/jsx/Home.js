57b0db6c12c52cad06bc3b6a1276bde8bf57a15e
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2021-08-01 22:45:54 +0545

    CSS Fix

--- src/components/jsx/Home.js
+++ src/components/jsx/Home.js
@@ -3,24 +3,25 @@ import notesImage from '../Images/notes.png';
 import adminImage from '../Images/admin.png';
 import stdImage from '../Images/std.png';
 import { Link } from 'react-router-dom';
+import '../styles/Home.css';
 
 const Home = () => {
   return (
-    <div className="h-100vh w-100% flex flex-col justify-center align-middle">
-      <h2 className="text-center text-gray-700 text-2xl font-semibold space-x-2">
+    <div className="flex flex-col items-center justify-center w-full h-screen bg-gray-800">
+      <h2 className="mt-5 text-center text-gray-700 text-4xl font-extrabold space-x-2">
         BCT-077 AB Portal
       </h2>
-      <div className="flex flex-wrap justify-between mt-40 w-9/12">
-        <Link to="/video">
+      <div className="flex flex-wrap justify-center mt-40 w-4/5">
+        <Link className={'navIcon'} to="/video">
           <img src={videoImage} alt="" />
         </Link>
-        <Link to="/notes">
+        <Link className={'navIcon'} to="/notes">
           <img src={notesImage} alt="" />{' '}
         </Link>
-        <Link to="/student">
+        <Link className={'navIcon'} to="/student">
           <img src={stdImage} alt="" />{' '}
         </Link>
-        <Link to="/admin">
+        <Link className={'navIcon'} to="/admin">
           <img src={adminImage} alt="" />{' '}
         </Link>
       </div>
@@ -28,20 +29,4 @@ const Home = () => {
   );
 };
 
-// const NavIcon = styled.img`
-//   width: 180px;
-//   object-fit: contain;
-//   margin: 30px;
-//   filter: drop-shadow(-5px 11px 4px rgba(0, 0, 0, 0.25));
-//   cursor: pointer;
-//   transition: all 0.3s cubic-bezier(0.165, 0.84, 0.44, 1);
-//   &:hover {
-//     filter: drop-shadow(-10px 11px 4px rgba(31, 34, 61, 0.925));
-//     transform: rotateX(-17deg);
-//   }
-//   &:active {
-//     transform: scale(1.2);
-//     filter: drop-shadow(-10px 11px 4px rgba(150, 50, 26, 0.925));
-//   }
-// `;
 export default Home;
